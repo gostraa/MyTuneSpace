585fc84ed8b35969ad9b1396183bba3b6d062cfc
Mariia <mcherkashina77@gmail.com>
2024-03-18 19:34:21 +0100

    change routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Home from "./screens/home";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter basename="/MyTuneSpace">
       <Home />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Library from "./library";
 import Feed from "./feed";
 import Trending from "./trending";
@@ -7,14 +7,12 @@ import Favorite from "./favorite";
 
 export default function Home() {
   return (
-    <Router basename="/MyTuneSpace">
-      <Routes>
-        <Route path="/" element={<Library />}></Route>
-        <Route path="/feed" element={<Feed />}></Route>
-        <Route path="/trending" element={<Trending />}></Route>
-        <Route path="/player" element={<Player />}></Route>
-        <Route path="/favorite" element={<Favorite />}></Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Library />}></Route>
+      <Route path="/feed" element={<Feed />}></Route>
+      <Route path="/trending" element={<Trending />}></Route>
+      <Route path="/player" element={<Player />}></Route>
+      <Route path="/favorite" element={<Favorite />}></Route>
+    </Routes>
   );
 }
