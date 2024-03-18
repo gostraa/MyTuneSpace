f2ea20111426a142b7be6919323e7d86888b53b5
Mariia <mcherkashina77@gmail.com>
2024-03-18 19:45:52 +0100

    change home and app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import "./App.css";
-import Home from "./screens/home";
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Library from "./screens/library";
+import Feed from "./screens/feed";
+import Trending from "./screens/trending";
+import Player from "./screens/player";
+import Favorite from "./screens/favorite";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter basename="/MyTuneSpace">
-      <Home />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Library />}></Route>
+      <Route path="/feed" element={<Feed />}></Route>
+      <Route path="/trending" element={<Trending />}></Route>
+      <Route path="/player" element={<Player />}></Route>
+      <Route path="/favorite" element={<Favorite />}></Route>
+      <Route path="*" element={<h2>not found</h2>} />
+    </Routes>
   );
 }
-
-export default App;
