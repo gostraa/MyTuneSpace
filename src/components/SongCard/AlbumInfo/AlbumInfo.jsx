e2407cc2596d0b17247230041237e020110d4b62
Mariia <mcherkashina77@gmail.com>
2024-04-05 22:28:42 +0200

    create AlbumInfo

--- src/components/SongCard/AlbumInfo/AlbumInfo.jsx
+++ src/components/SongCard/AlbumInfo/AlbumInfo.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 import "./albumInfo.css";
-export default function AlbumInfo() {
-  return <div>AlbumInfo</div>;
+export default function AlbumInfo({ album }) {
+  const artists = [];
+  album?.artists?.forEach((artist) => {
+    artists.push(artist.name);
+  });
+  console.log(artists);
+
+  return (
+    <div className="albumInfo-card">
+      <div className="albumName-container">
+        <p>{`${album?.name} - ${artists.join(", ")}`}</p>
+      </div>
+      <div className="album-info">
+        <p>{`${album?.name} is an ${album?.album_type} by ${artists.join(
+          ", "
+        )} with ${album?.total_tracks} track(s)`}</p>
+      </div>
+      <div className="album-release">
+        <p>Release Date: {album?.release_date}</p>
+      </div>
+    </div>
+  );
 }
